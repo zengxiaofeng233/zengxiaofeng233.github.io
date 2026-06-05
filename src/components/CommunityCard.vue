5224ff5c38acc8c158008a9a1027f8f2fecf59db
Yuki Wong <332635016@qq.com>
2026-06-05 09:22:34 +0800

    Simplify QQ group cards

--- src/components/CommunityCard.vue
+++ src/components/CommunityCard.vue
@@ -1,17 +1,7 @@
 <template>
   <article class="community-card">
-    <div class="community-icon" aria-hidden="true">
-      <img v-if="community.avatarUrl" :src="community.avatarUrl" alt="" referrerpolicy="no-referrer" />
-      <span v-else>QQ</span>
-    </div>
     <div class="community-body">
-      <div class="community-title-row">
-        <strong>{{ community.name }}</strong>
-        <StatusBadge :status="community.status" />
-      </div>
-      <span>{{ community.contactType }}：{{ community.contactValue }}</span>
-      <small>{{ community.type }} · {{ formatNumber(community.memberCount) }} 人 · {{ community.city }}</small>
-      <p>{{ community.description }}</p>
+      <strong>游客群：{{ community.contactValue }}</strong>
     </div>
     <button class="copy-button" type="button" @click="copyContact">
       {{ copied ? '已复制' : '复制群号' }}
@@ -22,8 +12,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import type { CommunityItem } from '../types'
-import { formatNumber } from '../utils/format'
-import StatusBadge from './StatusBadge.vue'
 
 const props = defineProps<{
   community: CommunityItem
